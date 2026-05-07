bdec43fd16ac3f617987ac8e5619830bf05e4970
Krish Bhagirath <94995518+krishbhagirath@users.noreply.github.com>
2026-05-07 17:02:24 -0400

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        # binary search is performed on POSSIBLE EATING SPEEDS
+        # eating speeds can be anywhere from 1 (min) to max value of piles
+
+        l = 1
+        r = max(piles)
+
+        while l <= r:
+            hours = 0
+            k = (l + r) // 2 # middle value
+
+            for pile in piles:
+                hours += (pile + k - 1) // k # calculate hours needed to complete
+            
+            if hours <= h:
+                # if we took less time than required (or equal), look at left half
+                result = k # this value works, store in result
+                r = k - 1 # keep checking for a potentially better speed (smaller value)
+            elif hours > h:
+                l = k + 1
+
+
+        return result
