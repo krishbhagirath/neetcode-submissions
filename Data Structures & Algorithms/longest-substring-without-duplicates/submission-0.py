5449f78d2c1b8d9e68ef10d76aa29d1dbebd0e87
Krish Bhagirath <94995518+krishbhagirath@users.noreply.github.com>
2026-05-29 00:23:16 -0400

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        r = 1
+        max = 0
+        substring = True            
+        
+        for i in range(len(s)):
+            seen = set()
+            innerIndex = 0
+            substring = True
+
+            while (i + innerIndex < len(s)):
+                if (s[i + innerIndex]) in seen:
+                    break
+
+                seen.add(s[i + innerIndex])
+                innerIndex += 1
+        
+            if (innerIndex > max):
+                max = innerIndex
+
+        
+        return max
+
+
+        # while l < r:
+        #     if ()
+
+
+        # start with first two pointers (l and r)
+        # outer loop for l - while l < r and the current state is not substring
+            # keep moving l fwd
+
+            # else
+            # while it is a substring
+                # move r fwd, update max if greater than current > max
